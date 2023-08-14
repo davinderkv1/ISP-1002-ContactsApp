c4a61258e10318c1ff40fa7f9c5dde478370683e
davinder <davinderkv1@rediffmail.com>
2023-08-14 14:50:17 -0400

    Created Project in Xcode

---
+++ Contacts App/Contacts App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts App
+//
+//  Created by user242096 on 8/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
